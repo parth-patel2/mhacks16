9e4233e67dc5bb43ee8085734dd8831dd3880136
Olive Ren <oliveren@Olives-MacBook-Air.local>
2023-11-18 17:32:29 -0500

    update react

--- roadtrip_ai/js/landingPage.jsx
+++ roadtrip_ai/js/landingPage.jsx
@@ -1,46 +1,90 @@
 import React from "react";
 import '../static/css/style.css';
 
+
+
+const JourneyQuote = () => (
+  <p className="life-is-a-journey">
+    Life is a journey
+    <br />
+    Not a destination.
+  </p>
+);
+
+const PersonalizeBanner = () => (
+  <div className="mountains">
+    <p className="personalize-your">PERSONALIZE YOUR TRIP WITH TECHNOLOGY</p>
+  </div>
+);
+
+const ActivityCart = () => (
+  <div className="text-wrapper">Activity Cart</div>
+);
+
+const DestinationLabel = () => (
+  <div className="label">Destination</div>
+);
+
+const StartLocationLabel = () => (
+  <div className="label-2">Start location:</div>
+);
+
+const TripTypeQuestion = () => (
+  <div className="frame">
+    <p className="p">What kind of trip are you looking for?</p>
+  </div>
+);
+
+const RoadTripAssistantLabel = () => (
+  <div className="label-3">Road trip assistant:</div>
+);
+
+const StartButton = () => (
+  <button className="button">
+    <div className="input-wrapper">
+      <div className="input">start</div>
+    </div>
+  </button>
+);
+
+const ImageIcons = () => (
+  <>
+    <img className="car-front" alt="Car front" src="car-front.svg" />
+    <img className="map-pin" alt="Map pin" src="map-pin.svg" />
+    <img className="shopping-cart" alt="Shopping cart" src="shopping-cart.svg" />
+    <img className="user-circle" alt="User circle" src="user-circle-2.svg" />
+  </>
+);
+
+const ScreenShot = () => (
+  <div className="screen-shot-wrapper">
+    <img className="screen-shot" alt="Screen shot" src="screen-shot-2023-11-18-at-3-03-1.png" />
+  </div>
+);
+
 export const LandingPage = () => {
   return (
     <div className="landing-page">
       <div className="div">
         <div className="overlap">
-          <p className="life-is-a-journey">
-            Life is a journey
-            <br />
-            Not a destination.
-          </p>
-          <div className="mountains">
-            <p className="personalize-your">PERSONALIZE YOUR TRIP WITH TECHNOLOGY</p>
-          </div>
+          <JourneyQuote />
+          <PersonalizeBanner />
           <div className="overlap-group">
             <div className="overlap-2">
               <div className="rectangle" />
               <div className="rectangle-2" />
-              <img className="car-front" alt="Car front" src="car-front.svg" />
-              <div className="label">Destination</div>
-              <div className="text-wrapper">Activity Cart</div>
-              <div className="label-2">Start location:</div>
+              <ImageIcons />
+              <DestinationLabel />
+              <ActivityCart />
+              <StartLocationLabel />
               <div className="rectangle-3" />
-              <div className="frame">
-                <p className="p">What kind of trip are you looking for?</p>
-              </div>
-              <div className="label-3">Road trip assistant:</div>
-              <img className="map-pin" alt="Map pin" src="map-pin.svg" />
-              <img className="shopping-cart" alt="Shopping cart" src="shopping-cart.svg" />
-              <img className="user-circle" alt="User circle" src="user-circle-2.svg" />
+              <TripTypeQuestion />
+              <RoadTripAssistantLabel />
             </div>
-            <button className="button">
-              <div className="input-wrapper">
-                <div className="input">start</div>
-              </div>
-            </button>
+            <StartButton />
           </div>
         </div>
-        <div className="screen-shot-wrapper">
-          <img className="screen-shot" alt="Screen shot" src="screen-shot-2023-11-18-at-3-03-1.png" />
-        </div>
+        <ScreenShot />
       </div>
     </div>
   );
