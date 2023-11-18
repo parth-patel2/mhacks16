55c8b127395f6bd3c1d32964f1cbecb65c8190a5
Parth Patel <partpat@umich.edu>
2023-11-18 17:18:45 -0500

    landing page displays on screen

--- roadtrip_ai/js/main.jsx
+++ roadtrip_ai/js/main.jsx
@@ -1,47 +1,14 @@
-import React from "react";
-import "./style.css";
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { LandingPage } from "./landingPage";
 
-export const MacbookPro = () => {
-  return (
-    <div className="macbook-pro">
-      <div className="div">
-        <div className="overlap">
-          <p className="life-is-a-journey">
-            Life is a journey
-            <br />
-            Not a destination.
-          </p>
-          <div className="mountains">
-            <p className="personalize-your">PERSONALIZE YOUR TRIP WITH TECHNOLOGY</p>
-          </div>
-          <div className="overlap-group">
-            <div className="overlap-2">
-              <div className="rectangle" />
-              <div className="rectangle-2" />
-              <img className="car-front" alt="Car front" src="car-front.svg" />
-              <div className="label">Destination</div>
-              <div className="text-wrapper">Activity Cart</div>
-              <div className="label-2">Start location:</div>
-              <div className="rectangle-3" />
-              <div className="frame">
-                <p className="p">What kind of trip are you looking for?</p>
-              </div>
-              <div className="label-3">Road trip assistant:</div>
-              <img className="map-pin" alt="Map pin" src="map-pin.svg" />
-              <img className="shopping-cart" alt="Shopping cart" src="shopping-cart.svg" />
-              <img className="user-circle" alt="User circle" src="user-circle-2.svg" />
-            </div>
-            <button className="button">
-              <div className="input-wrapper">
-                <div className="input">start</div>
-              </div>
-            </button>
-          </div>
-        </div>
-        <div className="screen-shot-wrapper">
-          <img className="screen-shot" alt="Screen shot" src="screen-shot-2023-11-18-at-3-03-1.png" />
-        </div>
-      </div>
-    </div>
-  );
-};
+// Create a root
+const root = createRoot(document.getElementById("reactEntry"));
+
+// This method is only called once
+// Insert the post component into the DOM
+root.render(
+  <StrictMode>
+    <LandingPage />
+  </StrictMode>,
+);
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const { existsSync } = require("fs");
+
+// Set the entrypoint to main.jsx by default, but main.tsx if using TypeScript.
+let entry = "./roadtrip_ai/js/main.jsx";
+if (existsSync("./roadtrip_ai/js/main.tsx")) {
+  entry = "./roadtrip_ai/js/main.tsx";
+}
+
+module.exports = {
+  mode: "development",
+  entry,
+  output: {
+    path: path.join(__dirname, "/roadtrip_ai/static/js/"),
+    filename: "bundle.js",
+  },
+  devtool: "source-map",
+  module: {    
+    rules: [
+      {
+        // Test for js or jsx files
+        test: /\.jsx?$/,
+        // Exclude external modules from loader tests
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          plugins: ["@babel/transform-runtime"],
+        },
+      },
+      {
+        // Support for TypeScript in optional .ts or .tsx files
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+};
